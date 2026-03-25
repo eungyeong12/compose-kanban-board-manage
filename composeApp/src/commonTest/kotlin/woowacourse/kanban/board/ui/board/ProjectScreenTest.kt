17e6e07c3d47edefd54d9a9c942bb9cd687d375e
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-03-25 17:24:57 +0900

    test: 프로젝트 제목 리스트가 노출되는지 검증

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/board/ProjectScreenTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/board/ProjectScreenTest.kt
@@ -32,6 +32,19 @@ class ProjectScreenTest {
         )
 
     @Test
+    fun `프로젝트 제목 리스트가 노출된다`() = runComposeUiTest {
+        setContent {
+            ProjectScreen(
+                projects = projects,
+                onProjectChange = { project, tasks -> projects[projects.indexOf(project)].tasks = tasks },
+            )
+        }
+
+        onNodeWithText("Compose1", useUnmergedTree = true).assertExists()
+        onNodeWithText("Compose2", useUnmergedTree = true).assertExists()
+    }
+
+    @Test
     fun `프로젝트 버튼을 클릭하면 해당 프로젝트의 태스크가 노출된다`() = runComposeUiTest {
         setContent {
             ProjectScreen(
