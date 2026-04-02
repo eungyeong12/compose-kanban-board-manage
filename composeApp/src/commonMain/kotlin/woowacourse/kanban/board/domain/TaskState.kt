98e26db7df2720bf8813e6d94c9093f4ec0c11f1
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-04-02 18:06:32 +0900

    feat: 상태 전이를 할 수 없는 경우 스낵바 노출

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/TaskState.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/TaskState.kt
@@ -5,4 +5,16 @@ enum class TaskState(val inAuthorRequired: Boolean, val isDeletable: Boolean) {
     IN_PROGRESS(true, true),
     REVIEW(true, false),
     DONE(true, false),
+    ;
+
+    companion object {
+        fun isAvailableTransition(from: TaskState, to: TaskState): Boolean {
+            return when (from) {
+                TO_DO -> to == IN_PROGRESS
+                IN_PROGRESS -> to == TO_DO || to == REVIEW
+                REVIEW -> to == IN_PROGRESS || to == DONE
+                DONE -> to == TO_DO
+            }
+        }
+    }
 }
